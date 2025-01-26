43ba4825d6d9a77e3cf67c100bdbd55ce2fee220
thescarecrxw <freestylerdude2006@gmail.com>
2025-01-26 03:59:53 -0500

    added recipe

--- scripts/HomePage.js
+++ scripts/HomePage.js
@@ -1,3 +1,4 @@
+//import OpenAI from '/node_modules/openai/index.mjs';
 import Page from "./Page.js";
 
 export default class HomePage extends Page {
@@ -18,18 +19,51 @@ export default class HomePage extends Page {
     const homePageDiv = document.createElement('div');
     homePageDiv.id = 'home-page';
 
+    //const openai = new OpenAI();
+
+    
+
     const textBox = document.createElement('input');
     textBox.type = 'text';
     textBox.placeholder = 'Enter text here';
     textBox.classList.add('home-page-textbox');
-
+    
+    textBox.addEventListener('keypress', (event) => {
+      if (event.key === 'Enter') {
+        event.preventDefault(); 
+        // this.getChatResponse(this.getText())
+        // console.log(this.getChatResponse(this.getText()))
+      }
+    });
 
     homePageDiv.appendChild(textBox);
     return homePageDiv;
   }
+
+  getText() {
+    const itemInput = document.getElementById('item-input');
+    const itemName = itemInput;
+    return itemName.value.trim();
+  }
+
+  // async getChatResponse(userInput){
+  //   const completion = await openai.chat.completions.create({
+  //     model: "gpt-4o",
+  //     messages: [
+  //         { role: "developer", content: "You are a helpful assistant." },
+  //         {
+  //             role: "user",
+  //             content: userInput,
+  //         },
+  //     ],
+  //     store: true,
+  // });
+  
+  //}
 
   init() {
     const pageContent = document.getElementById('page-content');
     pageContent.appendChild(this.render());
   }
 }
+
